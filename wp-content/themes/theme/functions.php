31cc1aa9661b98d3e33686365dd75b40349f96e9
CarlosVinicius3258 <carlinhosb13@live.com>
2020-08-26 12:28:45 -0300

    load js files

--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -5,3 +5,28 @@ function load_stylesheets(){
 }
 
 add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+
+
+//Footer functions
+function load_js(){
+    wp_register_script('jquery-min', get_template_directory_uri().'/assets/js/jquery.min.js', array(), 1,1,1);
+    wp_enqueue_script('jquery-min');
+
+    wp_register_script('jquery-scrollex', get_template_directory_uri().'/assets/js/jquery.scrollex.js', array(), 1,1,1);
+    wp_enqueue_script('jquery-scrollex');
+
+    wp_register_script('jquery-scrolly', get_template_directory_uri().'/assets/js/jquery.scrolly.js', array(), 1,1,1);
+    wp_enqueue_script('jquery-scrolly');
+
+    wp_register_script('skel', get_template_directory_uri().'/assets/js/skel.min.js', array(), 1,1,1);
+    wp_enqueue_script('skel');
+
+    wp_register_script('util', get_template_directory_uri().'/assets/js/util.js', array(), 1,1,1);
+    wp_enqueue_script('util');
+
+    wp_register_script('main', get_template_directory_uri().'/assets/js/main.js', array(), 1,1,1);
+    wp_enqueue_script('main');
+}
+
+add_action('wp_enqueue_scripts', 'load_js');
