d909fcd055938d893d70b949210b851697b6cdf3
CarlosVinicius3258 <carlinhosb13@live.com>
2020-09-03 01:37:34 -0300

    add footer elements on ACF

--- wp-content/themes/theme/footer.php
+++ wp-content/themes/theme/footer.php
@@ -1,11 +1,20 @@
 <!-- Footer -->
+<?php $footer = get_field('footer');?>
 <footer id="footer">
 				<div class="copyright">
 					<ul class="icons">
-						<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>
+					<?php if($footer['twitter_link']): ?>
+						<li><a href="<?php echo $footer['twitter_link']?>" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<?php endif; ?>
+					<?php if($footer['facebook_link']): ?>
+						<li><a href="<?php echo $footer['facebook_link']?>" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+					<?php endif; ?>	
+					<?php if($footer['instagram_link']): ?>
+						<li><a href="<?php echo $footer['instagram_link']?>" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<?php endif; ?>	
+					<?php if($footer['snapchat_link']): ?>
+						<li><a href="<?php echo $footer['snapchat_link']?>" class="icon fa-snapchat"><span class="label">Snapchat</span></a></li>
+					<?php endif; ?>
 					</ul>
 					<p>&copy; Untitled. All rights reserved. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a href="https://unsplash.com">Unsplash</a>.</p>
 				</div>
